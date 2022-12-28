98377790f3d482401c1b62405eb1e94fa106565e
Arky Asmal <arkyasmal@gmail.com>
2022-12-28 08:14:41 -0500

    Added skills page layout with responsive design

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,10 +1,12 @@
 import IntroBanner from "./components/introBanner/IntroBanner";
 import ProjectBanner from "./components/projectBanner/ProjectBanner";
+import SkillsBanner from "./components/skillsBanner/SkillsBanner";
 const HomePage = () => {
   return (
     <div className={"home-pg-container"}>
       {/* <IntroBanner /> */}
-      <ProjectBanner />
+      {/* <ProjectBanner /> */}
+      <SkillsBanner />
     </div>
   );
 };
