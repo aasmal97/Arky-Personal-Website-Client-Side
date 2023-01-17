efdecdec1d9172b96b42aa8c0484ac34fd19f3bc
aasmal97 <74555081+aasmal97@users.noreply.github.com>
2023-01-17 17:23:08 -0500

    finisshed pagination logic

--- src/pages/projectPage/ProjectPage.tsx
+++ src/pages/projectPage/ProjectPage.tsx
@@ -19,7 +19,111 @@ const waveStyles: { [key: string]: string } = {
 const calculateImgHeight = (waveHeight: number, headerHeight: number) => {
   return (waveHeight - headerHeight) * 1.8;
 };
+const getBtnArray = (
+  btnInterval: number,
+  activeBtn: number,
+  endBtn: number
+) => {
+  const arr = Array(btnInterval).fill(0);
+  const btnArr: number[] = [activeBtn];
+  let btnArrLeft = 0;
+  let btnArrRight = 0;
+  let currNum: number;
 
+  const addRight = () => {
+    btnArrRight++;
+    currNum = btnArrRight + activeBtn;
+    btnArr.push(currNum);
+  };
+  const addLeft = () => {
+    btnArrLeft++;
+    let currNum = activeBtn - btnArrLeft;
+    btnArr.unshift(currNum);
+  };
+  const iter = arr.length - 1;
+  for (let i = 0; i < iter; i++) {
+    if (arr.length % 2 !== 0) {
+      if (btnArr[btnArr.length - 1] + 1 <= endBtn) addRight();
+      else if (btnArr[0] - 1 > 0) addLeft();
+    } else {
+      if (btnArr[0] - 1 > 0) addLeft();
+      else if (btnArr[btnArr.length - 1] + 1 <= endBtn) addRight();
+    }
+    arr.pop();
+  }
+  return btnArr;
+};
+const PaginationBtns = ({
+  onChange,
+  endBtn,
+  activeBtn,
+  btnInterval,
+}: {
+  onChange?: (e: { prev: number; curr: number }) => void;
+  btnInterval: number;
+  endBtn: number;
+  activeBtn?: number;
+}) => {
+  const [active, setActive] = useState(
+    typeof activeBtn === "number" ? activeBtn : 1
+  );
+  const onBtnClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const target = e.currentTarget as HTMLElement;
+
+    const idx = target.dataset.idx ? parseInt(target.dataset.idx) : active;
+    setActive(idx);
+    if (onChange) onChange({ prev: active, curr: idx });
+  };
+  console.log(active);
+  //this allows for an active number to be passed in from outside
+  useEffect(() => {
+    if (typeof activeBtn === "number") setActive(activeBtn);
+  }, [activeBtn]);
+  const btnArr = getBtnArray(btnInterval, active, endBtn);
+  const ellipsis = (
+    <span className={`${namespace}-pagination-ellipsis`}>...</span>
+  );
+  //   console.log(btnArr[0] !== 1 || btnArr[0] - 1 !== 1)
+  return (
+    <div id={`${namespace}-pagination`}>
+      <button
+        onClick={onBtnClick}
+        data-idx={active - 1}
+        disabled={active - 1 <= 0}
+      >
+        Prev
+      </button>
+      {btnArr[0] !== 1 && (
+        <>
+          <button onClick={onBtnClick} data-idx={1}>
+            {1}
+          </button>
+          {btnArr[0] - 1 !== 1 && ellipsis}
+        </>
+      )}
+      {btnArr.map((idx) => (
+        <button key={idx} onClick={onBtnClick} data-idx={idx}>
+          {idx}
+        </button>
+      ))}
+      {btnArr[btnArr.length - 1] !== endBtn && (
+        <>
+          {btnArr[btnArr.length - 1] + 1 !== endBtn && ellipsis}
+          <button onClick={onBtnClick} data-idx={endBtn}>
+            {endBtn}
+          </button>
+        </>
+      )}
+      <button
+        disabled={active + 1 >= endBtn}
+        onClick={onBtnClick}
+        data-idx={active + 1}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
 export type ProjectCardProps = {
   generalURL?: string;
   appURL?: string;
@@ -77,7 +181,7 @@ const initialSlides: ProjectCardProps[] = [
     placeholderURL: " ",
     description: "This app does the following",
     githubURL: "https://github.com/aasmal97",
-    id: "dwedw",
+    id: "feferfre",
   },
 ];
 const ExploreAllBanner = ({ slides }: { slides: ProjectCardProps[] }) => {
@@ -92,6 +196,7 @@ const ExploreAllBanner = ({ slides }: { slides: ProjectCardProps[] }) => {
     </>
   );
 };
+
 const ProjectPage = () => {
   const [waveRef, waveSize] = useElementSize();
   const [headerRef, headerSize] = useElementSize();
@@ -100,6 +205,7 @@ const ProjectPage = () => {
   const params = useParams();
   const caroHeight = calculateImgHeight(waveSize.height, headerSize.height);
   const [slides, setSlides] = useState<ProjectCardProps[]>(initialSlides);
+  const [endBtn, setEndBtn] = useState(1);
   useEffect(() => {
     const idx = params.page ? parseInt(params.page) : 0;
     //we return to default settings
@@ -117,8 +223,12 @@ const ProjectPage = () => {
           <Carousel numSlidesPerView={1} namespace={namespace} />
         </div>
         <ExploreAllBanner slides={slides} />
+        <PaginationBtns
+          btnInterval={3}
+          endBtn={endBtn}
+          activeBtn={paginationIdx}
+        />
       </div>
-      <div id={`${namespace}-pagination`}></div>
     </div>
   );
 };
