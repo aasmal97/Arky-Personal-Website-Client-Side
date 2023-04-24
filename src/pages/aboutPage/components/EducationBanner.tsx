7746fc6a8fb29e54f758a87e3218048852609a32
Arky Asmal <arkyasmal@gmail.com>
2023-04-23 22:38:39 -0400

    added education data and links, along with finalized styling

--- src/pages/aboutPage/components/EducationBanner.tsx
+++ src/pages/aboutPage/components/EducationBanner.tsx
@@ -44,7 +44,7 @@ const Ellipse = ({
     zIndex: "0",
     ...style,
   };
-  
+
   const position = `${height / 2}${heightUnit}`;
   if (orientation === "top")
     styles["top"] = maxHeight
@@ -60,8 +60,55 @@ const Ellipse = ({
     </>
   );
 };
+const displayData = [
+  {
+    name: "P.S. 503",
+    description: "Elementary School",
+    imgLink: `${process.env.REACT_APP_MEDIA_FILES_URL}/educationLogos/PS503Logo.png`,
+    schoolLink: "https://www.ps503online.org/",
+  },
+  {
+    name: "Christa McAuliffe I.S. 187",
+    description: "Middle School",
+    imgLink: `${process.env.REACT_APP_MEDIA_FILES_URL}/educationLogos/IS187Logo.jpg`,
+    schoolLink: "https://www.is187nyc.com/",
+  },
+  {
+    name: "Prep For Prep",
+    description: "Prepartory School",
+    imgLink: `${process.env.REACT_APP_MEDIA_FILES_URL}/educationLogos/PrepForPrepLogo.png`,
+    schoolLink: "https://www.prepforprep.org/",
+  },
+  {
+    name: "Deerfield Academy",
+    description: "High School",
+    imgLink: `${process.env.REACT_APP_MEDIA_FILES_URL}/educationLogos/DeerfieldAcademyLogo.png`,
+    schoolLink: "https://deerfield.edu/",
+  },
+  {
+    name: "Cornell University",
+    description: "College",
+    imgLink: `${process.env.REACT_APP_MEDIA_FILES_URL}/educationLogos/CornellUniversityLogo.png`,
+    schoolLink: "https://www.cornell.edu/",
+  },
+];
 const EducationDisplay = () => {
-  return <div id={`${namespace}-display`}></div>;
+  return (
+    <div id={`${namespace}-display`}>
+      {displayData.map((item) => {
+        return (
+          <a
+            key={item.imgLink}
+            rel="noreferrer"
+            target="_blank"
+            href={item.schoolLink}
+          >
+            <img src={item.imgLink} alt={item.description + " logo"} />
+          </a>
+        );
+      })}
+    </div>
+  );
 };
 const EducationAction = () => {
   return (
