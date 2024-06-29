451311c6fb8b37bffb1e91edac569e0791cde051
Arky Asmal <arkyasmal@gmail.com>
2024-06-29 05:05:15 -0400

    added skills banner new styles

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -2,15 +2,14 @@ import IntroBanner from "./components/introBanner/IntroBanner";
 import ProjectBanner from "./components/projectBanner/ProjectBanner";
 import SkillsBanner from "./components/skillsBanner/SkillsBanner";
 import AboutMeBanner from "./components/aboutMeBanner/AboutMeBanner";
-import ContactMeBanner from "./components/contactBanner/ContactMe";
+// import ContactMeBanner from "./components/contactBanner/ContactMe";
 const HomePage = () => {
   return (
     <div className={"home-pg-container"}>
       {/* <IntroBanner /> */}
       {/* <ProjectBanner /> */}
-
-      <SkillsBanner />
-      {/* <AboutMeBanner /> */}
+      {/* <SkillsBanner /> */}
+      <AboutMeBanner />
       {/* <ContactMeBanner /> */}
     </div>
   );
--- src/pages/homePage/components/aboutMeBanner/AboutMeBanner.tsx
+++ src/pages/homePage/components/aboutMeBanner/AboutMeBanner.tsx
@@ -2,13 +2,15 @@ import { useEffect } from "react";
 import useWindowWidth from "../../../../hooks/useWindowWidth";
 import anime from "animejs";
 import useIntersectionWrapper from "../../../../hooks/useIntersectionWrapper";
-import { BlobSVG, MediaCollagePlaceholderSVG, MediaCollageSVG, WaveSVG } from "./SVGComponents";
+import {
+  BlobSVG,
+  MediaCollagePlaceholderSVG,
+  MediaCollageSVG,
+  WaveSVG,
+} from "./SVGComponents";
+import LinkBtn from "../../../../utilities/actionBtn/LinkBtn";
 const namespace = "about-me-pg";
-
-const TextContent = () => {
-  const { ref: textRef, isVisible } = useIntersectionWrapper();
-  const smallWindowWidth = useWindowWidth(992);
-  const title = "I’m also human!";
+const AboutMeHeader = ({ isVisible }: { isVisible: boolean }) => {
   useEffect(() => {
     const initialDelayTime = 500;
     if (isVisible)
@@ -22,64 +24,59 @@ const TextContent = () => {
       anime.remove(`.${namespace}-text-header .${namespace}-letter`);
     };
   }, [isVisible]);
+  const title = "I’m also human :)";
   return (
-    <div ref={textRef} className={`${namespace}-text-container`}>
-      <WaveSVG orientation={smallWindowWidth ? "right" : "top"} />
-      <div className={`${namespace}-text-content`}>
-        <h2
-          className={`${namespace}-text-header`}
-          aria-label={title}
-          style={{ position: "relative" }}
-        >
-          <span
-            className={`${namespace}-text-wrapper`}
-            style={{
-              position: "relative",
-              display: "inline-block",
-              overflow: "hidden",
-            }}
-          >
-            <span className={`${namespace}-letters`}>
-              {Array(title.length)
-                .fill(0)
-                .map((e, idx) => {
-                  return title[idx] !== " " ? (
-                    <span
-                      key={`${title[idx]}-${idx}`}
-                      className={`${namespace}-letter`}
-                      style={{
-                        transformOrigin: "0 0",
-                        display: "inline-block",
-                      }}
-                    >
-                      {title[idx]}
-                    </span>
-                  ) : (
-                    <span key={`${title[idx]}-${idx}`}>{title[idx]}</span>
-                  );
-                })}
-            </span>
-          </span>
-        </h2>
-        <span className={isVisible ? "visible" : ""}>
-          ( no not a Killers{" "}
-          <a
-            href="https://www.youtube.com/watch?v=RIZdjT1472Y"
-            rel="noopener noreferrer"
-            target={"_blank"}
-          >
-            reference
-          </a>{" "}
-          )
+    <h2
+      className={`${namespace}-text-header`}
+      aria-label={title}
+      style={{ position: "relative" }}
+    >
+      <span
+        className={`${namespace}-text-wrapper`}
+        style={{
+          position: "relative",
+          display: "inline-block",
+          overflow: "hidden",
+        }}
+      >
+        <span className={`${namespace}-letters`}>
+          {Array(title.length)
+            .fill(0)
+            .map((e, idx) => {
+              return title[idx] !== " " ? (
+                <span
+                  key={`${title[idx]}-${idx}`}
+                  className={`${namespace}-letter`}
+                  style={{
+                    transformOrigin: "0 0",
+                    display: "inline-block",
+                  }}
+                >
+                  {title[idx]}
+                </span>
+              ) : (
+                <span key={`${title[idx]}-${idx}`}>{title[idx]}</span>
+              );
+            })}
         </span>
-        <p className={isVisible ? "visible" : ""}>
-          <span>I</span> enjoy playing and watching football (sorry SOCCER for
-          my compatriots), along with chess and billiards. Don’t worry, this
-          doesn’t stem from my love of math or physics or anything... hehe. On
-          the odd day, you can also catch me accidentally butchering my mother’s
-          Ecuadorian recipes (After all, cooking is just chemistry right?
-          Ha...ha...)
-        </p>
+      </span>
+    </h2>
+  );
+};
+const TextContent = ({ isVisible }: { isVisible: boolean }) => {
+  const smallWindowWidth = useWindowWidth(992);
+
+  return (
+    <div className={`${namespace}-text-content`}>
+      <p className={`${isVisible ? "visible" : ""}`}>
+        <span>I</span> enjoy playing and watching football (sorry SOCCER for my
+        compatriots), along with chess and billiards. On the odd day, you can
+        also catch me accidentally butchering my mother’s Ecuadorian recipes
+        (After all, cooking is just chemistry... right?)
+      </p>
+      <div className={`${namespace}-action-btns`}>
+        <LinkBtn to="about">My Story</LinkBtn>
+        <LinkBtn to="/contact">Contact Me</LinkBtn>
       </div>
     </div>
   );
@@ -91,19 +88,20 @@ const MediaContent = () => {
     <div ref={mediaRef} className={`${namespace}-media-content`}>
       {isVisible && <MediaCollageSVG />}
       {!isVisible && <MediaCollagePlaceholderSVG />}
-      <div className={`${namespace}-media-inner`}>
-        <div className={`${namespace}-blob`}>{ <BlobSVG />}</div>
-      </div>
     </div>
   );
 };
 const AboutMeBanner = () => {
   const smallWindowWidth = useWindowWidth(992);
   const media = <MediaContent />;
+  const { ref: textRef, isVisible } = useIntersectionWrapper();
   return (
     <div id={namespace}>
-      {smallWindowWidth && media}
-      <TextContent />
+      <AboutMeHeader isVisible={isVisible} />
+      <div id={`${namespace}-inner-container`} ref={textRef}>
+        {smallWindowWidth && media}
+        <TextContent isVisible={isVisible} />
+      </div>
       {!smallWindowWidth && media}
     </div>
   );
