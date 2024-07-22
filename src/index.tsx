f25e0a54a1ce0e8e93906eaf8c0a324b119e6d3e
Arky Asmal <arkyasmal@gmail.com>
2024-07-22 07:43:53 -0400

    fixed fallback element

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider
+  RouterProvider,
 } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import Root from "./root";
@@ -17,39 +17,57 @@ const SkillsPage = React.lazy(() => import("./pages/skillsPage/SkillsPage"));
 const ContactPage = React.lazy(() => import("./pages/contactPage/ContactPage"));
 const FallbackElement = () => {
   const smallWindowWidth = useWindowWidth(576);
+  const mediumWindowWidth = useWindowWidth(992);
+
   return (
     <div
       style={{
-        paddingTop: "3.5em",
-        position: "absolute",
-        top: 0,
-        left: 0,
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         width: "100%",
         height: "100%",
-        minHeight: "100vh",
+        position: "absolute",
+        top: 0,
+        left: 0,
       }}
     >
-      <LoadingIcon
-        primaryFillColor={"#3AC2FF"}
-        secondaryFillColor={"#909090"}
-        faceFillColor={"#2e2e2e"}
-        strokeColor={"#2e2e2e"}
-        backgroundArmColor={"#2e2e2e"}
-        laptopLogoColor={"white"}
-        textColor={"white"}
-        width={!smallWindowWidth ? "80%" : "50%"}
-        background={{ color: "black" }}
-        center
-      />
+      <div
+        style={{
+          boxSizing: "border-box",
+          paddingTop: "3.5em",
+          paddingBottom: "3.5em",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+          minHeight: "100vh",
+          maxWidth: "1500px",
+        }}
+      >
+        <LoadingIcon
+          primaryFillColor={"#ff5050"}
+          secondaryFillColor={"#909090"}
+          faceFillColor={"#232323"}
+          strokeColor={"#232323"}
+          backgroundArmColor={"#232323"}
+          laptopLogoColor={"#909090"}
+          textColor={"#f2f2f2"}
+          width={!smallWindowWidth ? "80%" : !mediumWindowWidth ? "50%" : "40%"}
+          background={{ color: "#232323" }}
+          center
+        />
+      </div>
     </div>
   );
 };
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Root />}>
+    <Route path="/" element={
+      // <FallbackElement />
+      <Root />
+    }>
       <Route index element={<HomePage />} />
       <Route path="home/*" element={<HomePage />} />
       <Route path="projects" element={<ProjectPage />} />
