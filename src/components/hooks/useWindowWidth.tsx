e9cc2b3fb676f384a8fdaa0e00a8be2284663259
Arky Asmal <arkyasmal@gmail.com>
2025-01-02 00:27:51 -0500

    add outer width and inner width comparison logic to fix broken styles when a phone is rotated when using window width

--- src/components/hooks/useWindowWidth.tsx
+++ src/components/hooks/useWindowWidth.tsx
@@ -14,16 +14,19 @@ const useWindowWidth = (width = 576) => {
     let isMount = true;
     const resize = () => {
       if (!isMount) return;
-      if (window.innerWidth < width) setWidth(false);
-      else if (window.innerWidth >= width) setWidth(true);
+      if (window.innerWidth < width || window.outerWidth < width)
+        setWidth(false);
+      else if (window.innerWidth >= width && window.outerWidth >= width)
+        setWidth(true);
     };
-    const debouncedHandleResize = debounce(resize, 100);
+    const debouncedHandleResize = debounce(resize, 300);
     const cleanup = () => {
       window.removeEventListener("resize", debouncedHandleResize);
+      window.removeEventListener("orientationchange", debouncedHandleResize);
       isMount = false;
     };
     window.addEventListener("resize", debouncedHandleResize);
-
+    window.addEventListener("orientationchange", debouncedHandleResize);
     // Remove event listener on cleanup
     return () => cleanup();
   }, [width]);
